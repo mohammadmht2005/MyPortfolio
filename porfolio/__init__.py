573b30da186a9c76fee7fb7432f76c6679a758f8
mohammadmht2005 <mohammadmht2005@gmail.com>
2024-12-20 08:00:53 +0300

    Update __init__.py

--- porfolio/__init__.py
+++ porfolio/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask_login import LoginManager
 from flask_mail import Mail
-
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "ec29e9c6c1116fe18003de25ffa6805200e8711d42ab0cadce7e85b52d07b8c7"  
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'default-fallback-key')  
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 mail = Mail(app)
