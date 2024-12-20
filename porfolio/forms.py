c1ac55af51cca164de6d8b222bab5da665dae484
mohammadmht2005 <mohammadmht2005@gmail.com>
2024-12-20 08:10:53 +0300

    Delete porfolio/forms.py

--- porfolio/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField
-from wtforms.validators import DataRequired,Email,ValidationError
-
-class AddCourseForm(FlaskForm):
-    name = StringField('Course Name',validators=[DataRequired()])
-    author_id = StringField('Doctor Full Name',validators=[DataRequired()])
-    submit = SubmitField('Add Course')
