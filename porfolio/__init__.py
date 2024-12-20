e1830e30f9026c058f172199cbf242f98e7d07d7
mohammadmht2005 <mohammadmht2005@gmail.com>
2024-12-20 08:11:32 +0300

    Add files via upload

---
+++ porfolio/__init__.py
@@ -0,0 +1,7 @@
+from flask import Flask
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "ec29e9c6c1116fe18003de25ffa6805200e8711d42ab0cadce7e85b52d07b8c7"  
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+from portfolio import routes
